d50dde45084c72b33355046065d431e8414a24ec
Shashank Rajak <shashanksrajak@gmail.com>
2025-02-28 16:12:05 +0530

    lab 6 work

---
+++ lab_06/sample.py
@@ -0,0 +1,22 @@
+sentence = "i am in awe (fill with happy)"
+
+mood_emojis_dict = {
+    "happy": ["🙂", "🤣"]
+}
+
+if f"(fill with happy)" in sentence:
+    print("found an emoji")
+    sentence = sentence.replace(f"(fill with happy)", "")
+    words = sentence.split()
+else:
+    print("not found")
+
+print(words)
+print(sentence)
+
+words.append(" ".join(mood_emojis_dict["happy"]))
+
+
+print(words)
+
+print(" ".join(words))
